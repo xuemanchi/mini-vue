343e78c005e25c3a43ed1edbed69140f8a742668
manchixue <xuemanchi258@126.com>
2022-04-01 22:14:05 +0800

    feat(reactivity): add reactive item types

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -8,8 +8,16 @@ export const enum ReactiveFlags {
   RAW = '__v_raw',
 }
 
+export interface Target {
+  [ReactiveFlags.SKIP]?: boolean
+  [ReactiveFlags.IS_REACTIVE]?: boolean
+  [ReactiveFlags.IS_READONLY]?: boolean
+  [ReactiveFlags.IS_SHALLOW]?: boolean
+  [ReactiveFlags.RAW]?: any
+}
+
 const reactiveMap = new WeakMap()
-export function reactive<T extends object>(target: T) {
+export function reactive<T extends object>(target: T & Target) {
   if (!isObject(target))
     // 开发环境提示错误
     return
