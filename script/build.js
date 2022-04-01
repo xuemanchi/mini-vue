1286b009edf4c6327ae2e677d3a96fcfdce90b5a
manchixue <xuemanchi258@126.com>
2022-04-01 21:35:00 +0800

    chore: feat dev script to build a bundle

--- script/build.js
+++ script/build.js
@@ -4,26 +4,28 @@ const { resolve, relative } = require('path')
 const target = args._[0] || 'reactivity'
 const format = args.f || 'global'
 const outputFormat = format.startsWith('global')
-? 'iife'
-: format === 'cjs'
-? 'cjs'
-: 'esm'
+  ? 'iife'
+  : format === 'cjs'
+    ? 'cjs'
+    : 'esm'
 const pkg = require(resolve(__dirname, `../packages/${target}/package.json`))
 const outfile = resolve(
   __dirname,
-  `../packages/${target}/dist/${target}.js`
+  `../packages/${target}/dist/${target}.js`,
 )
 const relativeOutfile = relative(process.cwd(), outfile)
 build({
   entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
   outfile,
+  bundle: true,
   format: outputFormat,
   globalName: pkg.buildOptions?.name,
+  platform: format === 'cjs' ? 'node' : 'browser',
   watch: {
     onRebuild(error) {
       if (!error) console.log(`rebuilt: ${relativeOutfile}`)
-    }
-  }
+    },
+  },
 }).then(() => {
   console.log(`watching: ${relativeOutfile}`)
 })
