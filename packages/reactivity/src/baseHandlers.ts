fa4445fad6730ed25e0a89ddc4f1fea3800bd2e7
manchixue <xuemanchi258@126.com>
2022-04-17 16:15:29 +0800

    refactor: baseHandler

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -3,20 +3,31 @@ import { track, trigger } from './effect'
 import type { Target } from './reactive'
 import { ReactiveFlags } from './reactive'
 
-export const mutableHandlers = {
-  get(t: Target, key: string | symbol, receiver: object) {
+const get = createGetter()
+const set = createSetter()
+
+function createGetter(isReadonly = false) {
+  return function(t: Target, key: string | symbol, receiver: object) {
     if (key === ReactiveFlags.IS_REACTIVE)
       return true
     const r = Reflect.get(t, key, receiver)
     track(t, 'get', key)
     return r
-  },
-  set(t: Target, key: string | symbol, val: unknown, receiver: object) {
+  }
+}
+
+function createSetter() {
+  return function(t: Target, key: string | symbol, val: unknown, receiver: object) {
     const oldVal = t[key]
     const r = Reflect.set(t, key, val, receiver)
     if (oldVal !== val)
       trigger(t, 'set', key)
 
     return r
-  },
+  }
+}
+
+export const mutableHandlers = {
+  get,
+  set,
 }
